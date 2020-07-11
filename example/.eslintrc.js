2ab33388feba850063d3c5274aa5cb839aa5dca1
Osama <osamaqarem@gmail.com>
2020-07-11 12:06:20 +0800

    update: example app

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    'comma-dangle': ['never']
+  }
 };
