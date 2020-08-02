4443c15483921eb10241b9b0d97bc330d5fda6cb
Osama <osamaqarem@gmail.com>
2020-08-02 21:02:44 +0800

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -2,18 +2,7 @@ declare module "react-native-image-colors" {
   export function getColors<C extends Config>(
     url: string,
     config: C
-  ): Promise<
-    | (AndroidImageColors &
-        (C["dominant"] extends true ? { ["dominant"]: string } : {}) &
-        (C["average"] extends true ? { ["average"]: string } : {}) &
-        (C["vibrant"] extends true ? { ["vibrant"]: string } : {}) &
-        (C["darkVibrant"] extends true ? { ["darkVibrant"]: string } : {}) &
-        (C["lightVibrant"] extends true ? { ["lightVibrant"]: string } : {}) &
-        (C["darkMuted"] extends true ? { ["darkMuted"]: string } : {}) &
-        (C["lightMuted"] extends true ? { ["lightMuted"]: string } : {}) &
-        (C["muted"] extends true ? { ["muted"]: string } : {}))
-    | IOSImageColors
-  >;
+  ): Promise<AndroidImageColors | IOSImageColors>;
 }
 
 export type AndroidImageColors = {
@@ -33,19 +22,12 @@ export type IOSImageColors = {
   primary: string;
   secondary: string;
   detail: string;
+  quality: Config['quality']
   platform: "ios";
-  quality: "lowest" | "low" | "high" | "highest";
 };
 
 export type Config = {
-  defaultColor?: string;
-  dominant?: boolean;
-  average?: boolean;
-  vibrant?: boolean;
-  darkVibrant?: boolean;
-  lightVibrant?: boolean;
-  darkMuted?: boolean;
-  lightMuted?: boolean;
+  fallback?: string;
   pixelSpacing?: number;
-  muted?: boolean;
+  quality?: "lowest" | "low" | "high" | "highest";
 };
