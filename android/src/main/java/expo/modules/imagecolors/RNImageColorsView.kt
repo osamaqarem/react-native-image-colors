7222553fe4107b6f2aa3766ce3d66b1605a8b4e7
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-03-16 14:33:17 +0100

    chore: remove view files

--- android/src/main/java/expo/modules/imagecolors/RNImageColorsView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.imagecolors
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class RNImageColorsView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
