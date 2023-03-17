6d624979386d665d568be14bec755077c5101eb9
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-03-18 00:11:36 +0100

    feat(module): export functions

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,60 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
 // Import the native module. On web, it will be resolved to RNImageColors.web.ts
 // and on native platforms to RNImageColors.ts
+import { ImageRequireSource, Image } from 'react-native';
+
+import { ImageColorsResult, Config } from './RNImageColors.types';
 import RNImageColorsModule from './RNImageColorsModule';
-import RNImageColorsView from './RNImageColorsView';
-import { ChangeEventPayload, RNImageColorsViewProps } from './RNImageColors.types';
+import { cache } from './cache';
 
-// Get the native constant value.
-export const PI = RNImageColorsModule.PI;
+const MAX_KEY_LEN = 500;
 
-export function hello(): string {
-  return RNImageColorsModule.hello();
-}
+const resolveImageSource = (source: string | ImageRequireSource): string => {
+  if (typeof source === 'string') {
+    return source;
+  } else {
+    return Image.resolveAssetSource(source).uri;
+  }
+};
 
-export async function setValueAsync(value: string) {
-  return await RNImageColorsModule.setValueAsync(value);
-}
+/**
+ *
+ * @param uri - source of the image. Can be a remote URL, a base64 string or a local file path.
+ * ```ts
+ *  const fromUrl = await getColors('https://example.com/image.jpg')
+ *
+ *  const fromBase64 = await getColors('data:image/jpeg;base64,/9j/4Ri...')
+ *
+ *  const fromLocalFile = await getColors(require('./images/cat.jpg'))
+ * ```
+ * @param config - configuration
+ */
+const getColors = async (uri: string, config: Partial<Config>) => {
+  const resolvedSrc = resolveImageSource(uri);
 
-const emitter = new EventEmitter(RNImageColorsModule ?? NativeModulesProxy.RNImageColors);
+  if (config?.cache) {
+    const cachedResult = config.key
+      ? cache.getItem(config.key)
+      : cache.getItem(resolvedSrc);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
+    if (cachedResult) return cachedResult;
+  }
 
-export { RNImageColorsView, RNImageColorsViewProps, ChangeEventPayload };
+  const result: ImageColorsResult = await RNImageColorsModule.getColors(
+    resolvedSrc,
+    config
+  );
+
+  if (config?.cache) {
+    if (!config.key && resolvedSrc.length > MAX_KEY_LEN) {
+      throw new Error(
+        `You enabled caching, but you didn't pass a key. We fallback to using the image URI as the key. However the URI is longer than ${MAX_KEY_LEN}. Please pass a short unique key.`
+      );
+    }
+
+    cache.setItem(config.key ?? resolvedSrc, result);
+  }
+
+  return result;
+};
+
+export { getColors, cache };
